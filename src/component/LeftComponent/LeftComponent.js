6c09dd54041f15ccb54b4e67dc5f41e897cfddaa
sushantkushwaha <kushwaha.sushant@gmail.com>
2021-02-17 17:29:52 +0530

    Create LeftComponent.js

---
+++ src/component/LeftComponent/LeftComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LeftComponentBody from "./LeftComponentBody";
+import LeftComponentHeader from "./LeftComponentHeader";
+
+const LeftComponent = () => {
+  return (
+    <div>
+      <LeftComponentHeader />
+      <LeftComponentBody />
+    </div>
+  );
+};
+
+export default LeftComponent;
